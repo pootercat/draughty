e82f5b234517be2d20c883cd68ae111598565c0c
Dan Kurtz <dkurtz84@gmail.com>
2015-04-20 01:25:47 -0400

    add tests for player positions

--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -9,8 +9,32 @@ describe Player do
       Player.create(pname: 'Joe Montana', position: 'QB')
       Player.create(pname: 'Drew Bledsoe', position: 'QB')
       Player.create(pname: 'Dion Sanders', position: 'QB')
-      Pick.create(round: 1, pick:1)
+      Pick.create(round: 1, pick:1, team_id: Team.first['id'])
       expect(Player.undrafted.to_a.size).to eq 4
+    end
+
+    it 'should return unique list of positions' do
+      Player.create(pname: 'John Smith', position: 'QB')
+      Player.create(pname: 'Joe Montana', position: 'RB')
+      Player.create(pname: 'Drew Bledsoe', position: 'WR')
+      Player.create(pname: 'Dion Sanders', position: 'P')
+      expect(Player.positions.sort.first).to eq 'P'
+      expect(Player.positions.last).to eq 'P'
+      expect(Player.positions.size).to eq 4
+      Player.delete_all
+    end
+
+    it 'should return list of players available by position' do
+      Team.create(tname: 'Packers', division: 'NFC')
+      Player.create(pname: 'John Smith', position: 'QB')
+      Player.create(pname: 'Joe Montana', position: 'RB')
+      Player.create(pname: 'Drew Bledsoe', position: 'WR')
+      Player.create(pname: 'Dion Sanders', position: 'P')
+      Pick.create(round: 1, pick:1, team_id: Team.first['id'])
+      before_draft = Player.available_by_position('QB').to_a
+      Team.draft_player(Player.first['id'])
+      after_draft = Player.available_by_position('QB').to_a
+      expect(before_draft.size).to be > after_draft.size
     end
 
     it 'should validate presence of pname' do
